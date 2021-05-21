672156d430d0ab187d8ea62b1588579e071e4194
vishaljagtap3 <vishal.jagtap@bitcode.in>
2021-05-21 15:09:56 +0530

    Basics Listeners Notififydatasetchanged

--- app/src/main/java/in/bitcode/recyclerview1/CitiesAdapter.java
+++ app/src/main/java/in/bitcode/recyclerview1/CitiesAdapter.java
@@ -5,6 +5,9 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,7 +15,8 @@ import androidx.recyclerview.widget.RecyclerView;
 public class CitiesAdapter extends RecyclerView.Adapter<CitiesAdapter.CityViewHolder> {
 
     private Context mContext;
-    private String[] mArrCities;
+    //private String[] mArrCities;
+    private ArrayList<String> mListCities;
 
     class CityViewHolder extends RecyclerView.ViewHolder {
 
@@ -21,22 +25,39 @@ public class CitiesAdapter extends RecyclerView.Adapter<CitiesAdapter.CityViewHo
         public CityViewHolder(@NonNull View itemView) {
             super(itemView);
             mTxtCity = (TextView) itemView;
+
+            mTxtCity.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(
+                            mContext,
+                            "City clicked - " + mListCities.get(getAdapterPosition()),
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            });
         }
     }
 
-    public CitiesAdapter(Context context, String[] arrCities) {
+    public CitiesAdapter(Context context, /*String[] arrCities*/ ArrayList<String> listCities) {
         mContext = context;
-        mArrCities = arrCities;
+        //mArrCities = arrCities;
+        mListCities = listCities;
     }
 
     @Override
     public int getItemCount() {
         //Log.e("tag", "getItemCount " + mArrCities.length);
-        if (mArrCities == null) {
+       /* if (mArrCities == null) {
+            return 0;
+        }
+        return mArrCities.length;*/
+
+        if (mListCities == null) {
             return 0;
         }
 
-        return mArrCities.length;
+        return mListCities.size();
     }
 
     @NonNull
@@ -55,6 +76,15 @@ public class CitiesAdapter extends RecyclerView.Adapter<CitiesAdapter.CityViewHo
     @Override
     public void onBindViewHolder(@NonNull CitiesAdapter.CityViewHolder holder, int position) {
         Log.e("tag", "onBindViewHolder -- " + position);
-        holder.mTxtCity.setText(mArrCities[position]);
+        //holder.mTxtCity.setText(mArrCities[position]);
+        holder.mTxtCity.setText(mListCities.get(position));
+
+        //not the right place to setup listener to the view
+        /*holder.mTxtCity.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "City Clicked! " + mListCities.get(position), Toast.LENGTH_LONG).show();
+            }
+        });*/
     }
 }
--- app/src/main/java/in/bitcode/recyclerview1/MainActivity.java
+++ app/src/main/java/in/bitcode/recyclerview1/MainActivity.java
@@ -5,16 +5,27 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerCities;
+    /*
     private String [] mArrCities = {
             "Pune1", "Mumbai1", "Chennai1", "Delhi1", "Kolkata1", "Benguluru1", "Jaipur1", "Nagpur1", "Nashik1", "Kolhapur1",
             "Pune2", "Mumbai2", "Chennai2", "Delhi2", "Kolkata2", "Benguluru2", "Jaipur2", "Nagpur2", "Nashik2", "Kolhapur2",
             "Pune3", "Mumbai3", "Chennai3", "Delhi3", "Kolkata3", "Benguluru3", "Jaipur3", "Nagpur3", "Nashik3", "Kolhapur3",
     };
+     */
+    ArrayList<String> mListCities;
     private CitiesAdapter mCitiesAdapter;
+
+    private EditText mEdtCity;
+    private Button mBtnAdd, mBtnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListCities.add(0,mEdtCity.getText().toString());
+                //mCitiesAdapter.notifyDataSetChanged();
+                //mCitiesAdapter.notifyItemInserted(mListCities.size());
+                mCitiesAdapter.notifyItemInserted(0);
+            }
+        });
+
+        mBtnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = mListCities.indexOf(mEdtCity.getText().toString());
+                mListCities.remove(mEdtCity.getText().toString());
+                mCitiesAdapter.notifyItemRemoved(position);
+            }
+        });
     }
 
     private void init() {
@@ -29,12 +59,29 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager =
                 new LinearLayoutManager(
                         this,
-                        LinearLayoutManager.HORIZONTAL,
+                        LinearLayoutManager.VERTICAL,
                         false
                         );
         mRecyclerCities.setLayoutManager(linearLayoutManager);
 
-        mCitiesAdapter = new CitiesAdapter(this, mArrCities);
+        mListCities = new ArrayList<String>();
+        for(int i = 1; i<= 5; i++) {
+            mListCities.add("Pune"+i);
+            mListCities.add("Mumbai"+i);
+            mListCities.add("Chennai" + i);
+            mListCities.add("Delhi" + i);
+            mListCities.add("Kolkata" + i);
+            mListCities.add("Jaipur" + i);
+            mListCities.add("Nagpur" + i);
+        }
+
+
+        //mCitiesAdapter = new CitiesAdapter(this, mArrCities);
+        mCitiesAdapter = new CitiesAdapter(this, mListCities);
         mRecyclerCities.setAdapter(mCitiesAdapter);
+
+        mEdtCity = findViewById(R.id.edtCity);
+        mBtnAdd = findViewById(R.id.btnAdd);
+        mBtnDelete = findViewById(R.id.btnDelete);
     }
 }
